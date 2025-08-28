589f5eb79d8186728fe7f39b87586eea1ea782df
juancho87-bit <juan2532david@gmail.com>
2025-08-27 21:03:15 -0500

    nuevos cambios realizados al scrip

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //uno dos tres buenos cambios 
+p { 
+    front: size 140px;
+    front: size 140px;
+
+    front: size 140000px;front: size 14000px;
+}
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Documento para la rama live</title>
